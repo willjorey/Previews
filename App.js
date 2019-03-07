7e5f58636b486f1b530e13760ca75fac3cdf68cf
William <william.j.schober.reyes@gmail.com>
2019-03-07 14:02:26 -0500

    Popular movie posters displayed with styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import {searchMovies, getPopularMovies} from './src/api/fetch.js';
 
 export default class App extends React.Component {
@@ -15,18 +15,25 @@ export default class App extends React.Component {
     this.setState({
       popular: movies,
     });
-    console.log(this.state.popular[0].title)
+    console.log(this.state.popular[0].poster_path)
   };
 
   render() {
     return (
       <View style={styles.container}>
         <View>
-          <Text style={{marginTop: '20%'}}>Popular Movies</Text>
+          <Text style={{marginTop: '20%', textAlign:'center', padding: 10}}>Popular Movies</Text>
           <FlatList
             data={this.state.popular}
             keyExtractor={(item,index) => index.toString()}
-            renderItem={({item}) => <Text>{item.title}</Text>}
+            renderItem={({item}) => 
+            <View style={{flex:1, flexDirection: 'row', width: '90%', padding: 10}}>
+              <Image style={{width: 100, height: 175}} source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}/>
+              <View style={{justifyContent: 'center', width: '75%'}}>
+                <Text style={{fontSize: 20, fontWeight:'bold', marginLeft: '25%'}}>{item.title}</Text>
+              </View>
+            </View>
+            }
           />
         </View>
       </View>
